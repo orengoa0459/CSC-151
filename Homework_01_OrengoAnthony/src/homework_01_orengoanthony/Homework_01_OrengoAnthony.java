f1a75ab5c65a6e304c9277bc2b6dd53dcfad14cd
orengoa0459 <48802353+orengoa0459@users.noreply.github.com>
2020-04-08 07:44:23 -0400

    Add files via upload

--- Homework_01_OrengoAnthony/src/homework_01_orengoanthony/Homework_01_OrengoAnthony.java
+++ Homework_01_OrengoAnthony/src/homework_01_orengoanthony/Homework_01_OrengoAnthony.java
@@ -24,7 +24,7 @@ public class Homework_01_OrengoAnthony {
     
     //--------------Beginning of main method--------------------------//
     public static void main(String[] args) {
-                
+        pseudocode(); //Begins program with pseudocode       
         stock();// Calls the method stock()
         
     } 
@@ -77,21 +77,22 @@ public class Homework_01_OrengoAnthony {
                title, JOptionPane.INFORMATION_MESSAGE,tradeSymbol);
         
         //Get ticker/stock symbol from user
-        tickerSymbol = JOptionPane.showInputDialog(null, 
-                 "Enter Ticker Symbol: ","Ticker Symbol",JOptionPane.NO_OPTION);                                
+        tickerSymbol = (String) JOptionPane.showInputDialog(null, 
+                 "Enter Ticker Symbol: ","Ticker Symbol",JOptionPane.NO_OPTION,
+                 boxSymbol,null,"");                                
         
         //Get fill type from user (Market or Limit)
-        type = JOptionPane.showInputDialog(null, "Market/Limit", "Fill Type",
-                                                    JOptionPane.NO_OPTION);
+        type = (String)JOptionPane.showInputDialog(null, "Market/Limit", 
+                "Fill Type", JOptionPane.NO_OPTION,boxSymbol,null,"");
        
         //Get user to enter amount of ahres to be purchased
-        input = JOptionPane.showInputDialog(null,"How many shares : ","Shares",
-                                                    JOptionPane.NO_OPTION);
+        input = (String)JOptionPane.showInputDialog(null,"How many shares : "
+                + "","Shares", JOptionPane.NO_OPTION,boxSymbol,null,"");
         shares = Integer.parseInt(input); //Converts String to Integer//
         
         //Get user to enter current stock price
-        input = JOptionPane.showInputDialog(null,"Enter stock price: ","Shares",
-                                                    JOptionPane.NO_OPTION);        
+        input = (String)JOptionPane.showInputDialog(null,"Enter stock price: ",
+                "Shares",JOptionPane.NO_OPTION, boxSymbol,null,"");        
         stockPrice = Double.parseDouble(input);//Converts String to Double//
         
         
@@ -102,7 +103,7 @@ public class Homework_01_OrengoAnthony {
         
         
         //Object array to display stock purchase transaction
-        String [] stockPur = {
+        Object [] stockPur = {
            
            "******************************",
            "Order#:  " + order_Number,
@@ -119,8 +120,7 @@ public class Homework_01_OrengoAnthony {
            "Commission Fee:  " + dollar.format(purchaseFee),
            "Total Purchase Price :  " + dollar.format(totalPurchase),
            "***********************************"
-        };
-        
+        };        
             
             //Displays object array "stockPur in JOptionPane dialog box
             JOptionPane.showMessageDialog(null, stockPur,
@@ -129,13 +129,12 @@ public class Homework_01_OrengoAnthony {
        
        //*************End of stock purchase transaction********************//
        
-        
-        
+                
         /*This section of the program gives the user the option to sell 
         his/her shares or end the program.*/
         
             //Object array to display stock option menu
-            Object [] menu = 
+            Object [] optionsMenu = 
             {
                 
                 "Would you like to sell shares?",
@@ -144,16 +143,16 @@ public class Homework_01_OrengoAnthony {
             };
           
           //Displays object array "menu in JOptionPane dialog box  
-          String purchaseChoice = JOptionPane.showInputDialog(null, menu, 
+          String option = JOptionPane.showInputDialog(null, optionsMenu, 
                                       "*WARNING*",JOptionPane.OK_CANCEL_OPTION);
           //Converts purchaseOption to int
-          int purchaseOption = Integer.parseInt(purchaseChoice);
+          int optionChoice = Integer.parseInt(option);
           
           //Gives user the option to continue or not.
-          if(purchaseOption == 1)
+          if(optionChoice == 1)
           {
           }
-          else if(purchaseOption == 2){
+          else if(optionChoice == 2){
              JOptionPane.showMessageDialog(null, "Good Bye!"); 
              //Ends program
              System.exit(0);
@@ -165,27 +164,56 @@ public class Homework_01_OrengoAnthony {
         double percentShares;
         double sellPrice;     
         double sellAmount;  
+        double shareNumber;
        
-        
        //Welcome back dialog box  
       JOptionPane.showMessageDialog(null,"Welcome Back to Orengo Trade",
-                title, JOptionPane.OK_CANCEL_OPTION,tradeSymbol);  
-      
-     //tickerSymbol = JOptionPane.showInputDialog(null,"Enter Ticker Symbol: "); 
-        
+                title, JOptionPane.OK_CANCEL_OPTION,tradeSymbol);          
+              
         // Get current stock price from user
-       input = JOptionPane.showInputDialog(null,"Enter stock price: ",
-                                  "Current Stock Price",JOptionPane.NO_OPTION);        
+       input = (String)JOptionPane.showInputDialog(null,"Enter stock price: ",
+               "Current Stock Price",JOptionPane.NO_OPTION,boxSymbol,null,"");        
        stockPrice = Double.parseDouble(input); 
-        
-       //input = JOptionPane.showInputDialog(null,"Enter number of shares: ");
-       // shares = Integer.parseInt(input);
-        
-       //Get percentage of shares the user wants to sell
-        input = JOptionPane.showInputDialog(null,
-                                    "What percentage would you like to sell? ", 
-                                    "Percent of Shares",JOptionPane.NO_OPTION);
-        percentShares = Double.parseDouble(input);
+       
+        // Object array to display menu option for percentage and number
+        Object []shareMenu = 
+            {
+                
+                "Would you like to enter a number or percentage of shares?",
+                "Enter 1: Number",
+                "Enter 2: Percent"
+            };
+      
+          //Displays object array "menu in JOptionPane dialog box  
+          String shareChoice = (String)JOptionPane.showInputDialog(null, 
+                  shareMenu, " Share number",JOptionPane.OK_CANCEL_OPTION,
+                  boxSymbol,null,"");
+          
+          //Converts shareChoice to int
+          int shareOption = Integer.parseInt(shareChoice);
+          percentShares = 0;
+          //Gives user the option t enter a number or percentage of shares.
+          if(shareOption == 1)
+          {
+              //Get number of shares user wants to sell
+            input = (String)JOptionPane.showInputDialog(null, 
+                 "Enter number of shares","Shares",JOptionPane.OK_CANCEL_OPTION,
+                 boxSymbol,null,"");
+            shareNumber = Integer.parseInt(input);
+            //Converts number of shares inputted to a percentage
+            percentShares = (shareNumber / shares) * 100;           
+          }
+          else if(shareOption == 2)
+          
+          {
+             //Get percentage of shares the user wants to sell
+             input = (String)JOptionPane.showInputDialog(null,
+                   "What percentage would you like to sell? ", 
+                   "Percent of Shares",JOptionPane.NO_OPTION,boxSymbol,null,"");
+             percentShares = Double.parseDouble(input);
+           
+          }   
+                        
         
         //Calculates the percentage of shares to be sold
         double sharesPercent = percentShares / 100;
@@ -201,7 +229,7 @@ public class Homework_01_OrengoAnthony {
         /*Calculates the total profit by subtracting tade fees and 
         purchase price from total sell.*/
         double totalSell = sell - sellFee;
-        double totalFees= sellFee+ purchaseFee;
+        double totalFees = sellFee+ purchaseFee;
         double profit = (totalSell - stockPurPercent); 
         
         //Totals shares left over//
@@ -210,11 +238,11 @@ public class Homework_01_OrengoAnthony {
         double currentValue = sharesRemaining * stockPrice; 
         
         //Object array to display stock sell transaction
-         String[] stockSel = {
+         Object[] stockSel = {
            
            "******************************",
            "Order#:  " + order_Number2,
-           "Filled:  " + "Limit",
+           "Filled:  " + type,
            "Ticker Symbol:  " + tickerSymbol,
            "",
            "Account:  " + "OrengoA0000",
@@ -229,10 +257,11 @@ public class Homework_01_OrengoAnthony {
            "***********************************"
        };      
        
-       
+       //Displays sell transaction
       JOptionPane.showMessageDialog(null, stockSel,"Stock Sell Transaction",
                                    JOptionPane.INFORMATION_MESSAGE,boxSymbol);
       
+      // Object array to display menu option to sell or not
       Object [] menuSell = {
                 
             "Do you wish to continue",
@@ -289,4 +318,87 @@ public class Homework_01_OrengoAnthony {
     //================Ending of stock method================================//          
     
      
+    
+    
+    //**************************Pseudocode**********************************//
+    
+    public static void pseudocode(){
+        
+        ImageIcon tradeSymbol = new ImageIcon("OTResize.png");
+        String [] pseudo = new String []
+        {
+            
+           "                                                        "
+                + "     * Pseudocode *", 
+           "------------------------------------------------------------------"
+                + "-----------------------------------------------------------",
+           "1. Import JOptionPane, Decimal format, time/date format, "
+                + "Random, Image.",
+           " ",
+           "2. Declare/ initialize Decimal format, time/date format, "
+                + "Random, Image variables.",
+           " ",
+           "3. Declare stock trade variables and include one constant variable"
+                + "(trade fee).",
+           " ",
+           "4. Create a welcome box for the program and change image icon to "
+                + "OTResize.png",
+           " ",
+           "5. Get user input by creating input dialog boxes for ticker symbol,"
+                + " order type,",
+                           
+           "number of shares, and current stock price.",
+           " ",
+           "6. Declare variables to calculate the total cost to include fees.",
+           " ",
+           "7. Place variables in an object array called stockPur, and "
+                + "display it ",
+           "using a message dialog box. Note: format the object array to "
+                + "look like a reciet.",
+           " ",
+           "8. Create a menu from an object array  called optionsMenu, to give "
+                + "the user an option to sell. ",
+            " Note: If user chooses yes proceed to sell transaction, "
+                + "if user chooses no end program.",
+            " ",
+           "9. Create welcome back dialog box to begin the sell transaction",
+           " ",
+           "10. Declare sell variables.",
+           " ",
+           "11. Get user to input updated and current stock price.",
+           " ",
+           "12. Create object array menu called shareMenu, to give "
+                + "user an option to ",          
+           "enter a number or percentage of shares.",
+           " ",
+           "13.Declare sell variables to calculate the total cost of "
+                + "stock sell.",
+           " ",
+           "14. Calculate total profit by multiplying the total purchase by the"
+                + "percentage used to sell shares.  ",
+           " ",
+           "15. Place variables in an object array called stockSel, and "
+                + "display it ",           
+           "using a message dialog box. Note: format the object array to "
+                + "to look like a receipt.",     
+           " ",
+           "16. Create an object array called menuSell and give user an "
+                + "option to sell or not.",
+            " ", 
+           "17. Create a final object array called transactionSum and place the"
+                + "stockPur, stockSel arrays",
+           "plus the total profit, remaining shares and current account value."
+                + " Note: Display like a receipt.",
+            "================================================================="
+                + "=============="
+              
+        };
+        
+        JOptionPane.showMessageDialog(null, pseudo, "Pseudocode", 
+                JOptionPane.NO_OPTION,tradeSymbol);
+        
+        
+    }//*********************Pseudocode***************************************//
+    
+    
 }//Ending of Homework_01_OrengoAnthony//
